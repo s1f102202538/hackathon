d3c5384a74f0497ef12b9a384f7d0bc0213497e1
1F10220182 <s1f102201829@iniad.org>
2024-11-04 02:53:51 +0000

    make navbar

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './components/layout/navbar/page';
 
 const Home = () => (
   <div>
     <h1>Home</h1>
     <div>Content</div>
+    <div>success</div>
+    <div> jkdjfoisjasjf </div>
+    <div>keke </div>
+    <Navbar />
   </div>
 );
 
