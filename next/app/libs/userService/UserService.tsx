13afb7ccd163d8bdb4d3562fec7d7b1a02dec561
s1f102202538 <s1f102202538@iniad.org>
2024-11-05 23:47:40 +0900

    ユーザ作成API

---
+++ next/app/libs/userService/UserService.tsx
@@ -0,0 +1,14 @@
+import { PrismaClient, User } from '@prisma/client';
+
+export default class UserService {
+  private static readonly prisma = new PrismaClient();
+
+  public static async Create(clientId: string): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: {
+        clientId,
+      },
+    });
+    return user;
+  }
+}
