6f72a01735815fb5963e20ad10d5d95a6ac199a7
s1f102202538 <s1f102202538@iniad.org>
2024-11-06 15:45:09 +0900

    ユーザの単語リスト取得APIの作成

--- next/app/api/user/word/extraction/route.ts
+++ next/app/api/user/word/extraction/route.ts
@@ -5,30 +5,30 @@ import DeepLService from 'app/libs/apiService/DeepLService';
 import UserWordsService from 'app/libs/userService/UserWordsService';
 import { Word } from 'app/types/Word';
 
-export type GetWordsParams = {
+export type ExtractionWordsParams = {
   clientId: string;
   content: string;
 };
 
-export type GetWordsResponse = {
-  wordsArray: Word[];
+export type ExtractionWordsResponse = {
+  wordsList: Word[];
 };
 
-export async function POST(req: NextRequest): Promise<NextResponse<GetWordsResponse>> {
-  const params: GetWordsParams = await req.json();
+export async function POST(req: NextRequest): Promise<NextResponse<ExtractionWordsResponse>> {
+  const params: ExtractionWordsParams = await req.json();
 
   const jp = await OpenAIService.Ask(params.content);
   const en = await DeepLService.TranslatorWordsArray(jp, 'EN');
-  const wordsArray: Word[] = [];
+  const wordsList: Word[] = [];
   for (let i = 0; i < jp.length; i++) {
     const word: Word = {
       ja: jp[i],
       en: en[i],
     };
     UserWordsService.Create(params.clientId, word);
-    wordsArray.push(word);
+    wordsList.push(word);
   }
   return NextResponse.json({
-    wordsArray,
+    wordsList,
   });
 }
